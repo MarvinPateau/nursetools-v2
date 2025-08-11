a4b0418a893b8bee39d05efa90c92cb354ca5c6a
Marvin Pateau <62592318+MarvinPateau@users.noreply.github.com>
2025-08-11 18:14:16 +0200

    chore: configure tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import NurseToolkitApp from './App';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(<NurseToolkitApp />);
